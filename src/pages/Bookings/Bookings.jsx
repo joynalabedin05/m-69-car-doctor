055ba0f43d4370d1bf28d9a24b04bf90ee0860ab
joynalabedin05 <joynal05101993@gmail.com>
2023-05-13 08:54:36 +0600

    jwt added

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -2,16 +2,32 @@ import { useContext, useEffect } from "react";
 import { AuthContext } from "../../Providers/AuthProviders";
 import { useState } from "react";
 import BookingsRow from "./BookingsRow";
+import { useNavigate } from "react-router-dom";
 // "alt + shift + f" ,to format code
 const Bookings = () => {
     const { user } = useContext(AuthContext);
     const [bookings, setBookings] = useState([]);
+    const navigate = useNavigate();
     const url = `http://localhost:5000/bookings?email=${user?.email}`;
     useEffect(() => {
-        fetch(url)
+        fetch(url,{
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('car-access-token')}`
+            }
+
+            })
             .then(res => res.json())
-            .then(data => setBookings(data))
-    }, [url]);
+            
+            .then(data => {
+                if(!data.error){
+                    setBookings(data)
+                }
+                else{
+                    navigate('/')
+                }          
+            })
+    }, [url, navigate]);
 
     const handleDelete = id =>{
         const proceed = confirm('are you sure you want to delete');
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 
-import { Link } from 'react-router-dom';
+import { Link,  useLocation, useNavigate } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
 import { useContext } from 'react';
 import { AuthContext } from '../../Providers/AuthProviders';
@@ -7,16 +7,37 @@ import useTitle from '../../hooks/useTitle';
 const Login = () => {
     const {signIn}=useContext(AuthContext);
     useTitle('login');
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+    const navigate = useNavigate();
     const handleLogin = event=>{
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email,password);
+        // console.log(email,password);
         signIn(email,password)
         .then(result=>{
-            const loggedUser = result.user;
-            console.log(loggedUser)
+            const user = result.user;
+            const loggedUser = {
+                email : user.email
+            }
+            console.log(loggedUser);
+            
+            fetch('http://localhost:5000/jwt',{
+                method: 'POST',
+                headers:{
+                    'content-type':'application/json'
+                },
+                body: JSON.stringify(loggedUser)
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log('jwt response',data);
+                // localstorage is not the best, it is second best to access token
+                localStorage.setItem('car-access-token', data.token);
+                navigate(from, { replace: true });
+            })
         })
         .catch(error=>{
             console.log(error);
